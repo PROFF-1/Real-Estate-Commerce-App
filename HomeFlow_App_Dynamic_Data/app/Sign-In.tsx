fea4abf39441e591f0ad3a21e9cbb0cd6f99bace
PROFF-1 <richmondnyamedor04@gmail.com>
2025-04-22 17:09:31 +0000

    Google Authencation established

--- HomeFlow_App_Dynamic_Data/app/Sign-In.tsx
+++ HomeFlow_App_Dynamic_Data/app/Sign-In.tsx
@@ -1,13 +1,22 @@
-import { View, Text, ScrollView, Image, TouchableOpacity} from 'react-native'
+import { View, Text, ScrollView, Image, TouchableOpacity, Alert} from 'react-native'
 import React from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 
 import  images  from '@/constants/images'
 import icons from '@/constants/icons'
+import { login } from '@/lib/appwrite'
 
 const SignIn = () => {
 
-  const handleLogin = () => {}
+  const handleLogin = async () => {
+    const results = await login();
+
+    if(results){
+      console.log('Login successful')
+    }else{
+      Alert.alert(' Error', 'Failed to login')
+    }
+  }
   return (
     <SafeAreaView className='bg-white h-full'>
       <ScrollView contentContainerClassName='h-full'>
